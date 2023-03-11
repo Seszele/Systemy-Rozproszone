0872c7254b97f45aaa3072f1af23538ce2fbc136
Wojtek <Seszele11@onet.pl>
2023-03-11 22:04:03 +0100

    8pts

--- lab1/ChatClient/Program.cs
+++ lab1/ChatClient/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
 
 namespace ChatClient
 {
@@ -12,76 +10,119 @@ namespace ChatClient
         {
             StartClient();
         }
-
         static void StartClient()
         {
             string serverAddress = "localhost";
             IPAddress ipAddress = Dns.GetHostEntry("127.0.0.1").AddressList[0];
             int serverPort = 8888;
-            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            client.Connect(new IPEndPoint(ipAddress, serverPort));
-            Console.WriteLine($"Connected to server at {serverAddress}:{serverPort}");
 
-            Thread thread = new Thread(() => ReceiveMessages(client));
+            // TCP client socket
+            Socket tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpClient.Connect(new IPEndPoint(ipAddress, serverPort));
+            Console.WriteLine($"Connected to server at tcp->{serverAddress}:{serverPort}");
+
+            // UDP socket
+            Socket udpClient = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpClient.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpClient.Bind(new IPEndPoint(ipAddress, 0));
+            Console.WriteLine($"Created UDP endpoint at {udpClient.LocalEndPoint.ToString()}");
+            Console.Write($"Enter your nickname: ");
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                return;
+            }
+            input += "<|UDP|>" + udpClient.LocalEndPoint.ToString();
+            byte[] nickMessageBytes = Encoding.ASCII.GetBytes(input);
+            tcpClient.Send(nickMessageBytes);
+
+            Thread thread = new Thread(() => ReceiveMessages(tcpClient, udpClient));
             thread.Start();
 
             while (true)
             {
-                string? message = Console.ReadLine();
-                byte[] messageBytes = Encoding.ASCII.GetBytes(message ?? String.Empty);
-                if (client.Connected)
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
                 {
-                    client.Send(messageBytes);
+                    continue;
+                }
+
+                // If the input starts with "/U ", send a UDP message
+                if (input.StartsWith("/U "))
+                {
+                    string udpMessage = input.Substring(3);
+                    byte[] messageBytes = Encoding.ASCII.GetBytes(udpMessage);
+                    udpClient.SendTo(messageBytes, new IPEndPoint(ipAddress, serverPort));
                 }
                 else
                 {
-                    Console.WriteLine("Unable to send message, you are not connected to the server");
-                    break;
+                    // Otherwise, send a TCP message
+                    byte[] messageBytes = Encoding.ASCII.GetBytes(input);
+                    tcpClient.Send(messageBytes);
                 }
             }
         }
 
-        static void ReceiveMessages(Socket client)
+        static void ReceiveMessages(Socket tcpClient, Socket udpClient)
         {
             try
             {
                 byte[] buffer = new byte[1024];
                 int bytesRead;
 
-                while ((bytesRead = client.Receive(buffer)) > 0)
+                while (true)
                 {
-                    // if message contains <|SRV|>, it is a server message
-                    string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                    if (message.Contains("<|SRV|>"))
+                    // Receive TCP message
+                    if (tcpClient.Poll(1000, SelectMode.SelectRead))
                     {
-                        message = message.Replace("<|SRV|>", "");
-                        Console.Write("Server: " + message);
+                        bytesRead = tcpClient.Receive(buffer);
+                        if (bytesRead > 0)
+                        {
+                            //if message contains <| SRV |>, it is a server message 
+                            string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                            if (message.Contains("<|SRV|>"))
+                            {
+                                message = message.Replace("<|SRV|>", "Server:");
+                            }
+                            if (message.Contains("<|ACK|>"))
+                            {
+                                message = message.Replace("<|ACK|>", "You can start chatting now!");
+                            }
+                            Console.WriteLine(message);
+
+                        }
                     }
-                    else if (message.Contains("<|ACK|>"))
+
+                    // Receive UDP message
+                    EndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                    if (udpClient.Poll(1000, SelectMode.SelectRead))
                     {
-                        message = message.Replace("<|ACK|>", "");
-                        Console.WriteLine("You can start chatting now!");
+                        bytesRead = udpClient.ReceiveFrom(buffer, ref remoteEndPoint);
+                        if (bytesRead > 0)
+                        {
+                            string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                            Console.WriteLine(message);
+                        }
                     }
-                    else
-                    {
-                        Console.WriteLine(message);
-                    }
+
+                    Thread.Sleep(100);
                 }
             }
             catch (SocketException e)
             {
-                Console.WriteLine("Error receiving data from server: {0}, Server closed", e.ErrorCode);
+                Console.WriteLine($"Error receiving data from server: {e.ErrorCode}, Server closed");
             }
             catch (Exception e)
             {
-                Console.WriteLine("Error receiving data from server: {0}", e.Message);
+                Console.WriteLine($"Error receiving data from server: {e.Message}");
                 return;
             }
 
             Console.WriteLine("Disconnected from server");
 
-            client.Shutdown(SocketShutdown.Both);
-            client.Close();
+            tcpClient.Shutdown(SocketShutdown.Both);
+            tcpClient.Close();
+            udpClient.Close();
             Environment.Exit(0);
         }
     }
--- lab1/ChatServer/Program.cs
+++ lab1/ChatServer/Program.cs
@@ -1,14 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
 
 namespace ChatServer
 {
     class Program
     {
+        public class ClientInfo
+        {
+            public ClientInfo(Socket tcpClient, EndPoint udpEndpoint)
+            {
+                TcpClient = tcpClient;
+                UdpEndpoint = udpEndpoint;
+            }
+            public Socket TcpClient { get; set; }
+            public EndPoint UdpEndpoint { get; set; }
+
+            public override int GetHashCode()
+            {
+                return TcpClient.GetHashCode() ^ UdpEndpoint.GetHashCode();
+            }
+
+            public override bool Equals(object? obj)
+            {
+                if (obj == null)
+                {
+                    return false;
+                }
+                if (!(obj is ClientInfo))
+                {
+                    return false;
+                }
+                ClientInfo clientInfo = (ClientInfo)obj;
+                return clientInfo.TcpClient == TcpClient && clientInfo.UdpEndpoint == UdpEndpoint;
+            }
+        }
         static void Main(string[] args)
         {
             StartServer();
@@ -17,44 +43,62 @@ namespace ChatServer
         static void StartServer()
         {
             int port = 8888;
+            // TCP listener socket
             Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             listener.Bind(new IPEndPoint(IPAddress.Any, port));
             listener.Listen(100);
-            Console.WriteLine($"Server started on port {port}");
+            Console.WriteLine($"Server started on tcp port {port}");
+
+            // UDP socket
+            Socket udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            udpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+            Console.WriteLine($"Server started on udp port {port}");
 
             while (true)
             {
-                Socket client = listener.Accept();
-                client.Send(Encoding.ASCII.GetBytes("<|SRV|>enter your nickname: "));
+                Socket tcpClient = listener.Accept();
                 byte[] buffer = new byte[1024];
-                int bytesRead = client.Receive(buffer);
-                string nickname = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                Console.WriteLine($"Client {nickname} connected from {((IPEndPoint?)client.RemoteEndPoint)?.Address}");
+                int bytesRead = tcpClient.Receive(buffer);
+                string initialMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                // get nick and udp endpoint from the initial message
+                string nickname = initialMessage.Substring(0, initialMessage.IndexOf("<|UDP|>"));
+                string udpEndPoint = initialMessage.Substring(initialMessage.IndexOf("<|UDP|>") + 7);
+                EndPoint udpEndPointParsed = new IPEndPoint(IPAddress.Parse(udpEndPoint.Split(':')[0]), int.Parse(udpEndPoint.Split(':')[1]));
+                Console.WriteLine($"Client {nickname} connected");
 
                 // add the client to the list of connected clients
+                ClientInfo clientInfo = new ClientInfo(tcpClient, udpEndPointParsed);
+
                 lock (connectedClientsLock)
                 {
-                    connectedClients.Add(client, nickname);
+                    connectedClients.Add(clientInfo, nickname);
                 }
-                Thread thread = new Thread(() => HandleClient(client));
-                thread.Start();
+                // start a new thread to handle the tcp client
+                Thread tcpThread = new Thread(() => HandleClient(clientInfo));
+                tcpThread.Start();
+
+                // start a new thread to handle the udp client
+                Thread udpThread = new Thread(() => HandleUdpClient(clientInfo, udpSocket));
+                udpThread.Start();
             }
         }
 
-        static void HandleClient(Socket client)
+        static void HandleClient(ClientInfo clientInfo)
         {
-            // send an ACK message to the client
-            client.Send(Encoding.ASCII.GetBytes("<|ACK|>"));
+            Socket client = clientInfo.TcpClient;
+            string nickname = connectedClients[clientInfo];
             // send a message to all other connected clients
-            string? nickname;
-            lock (connectedClientsLock)
-            {
-                connectedClients.TryGetValue(client, out nickname);
-            }
-            SendToAllClients($"{nickname} has joined the chat", client);
+            SendToAllClients($"{nickname} has joined the chat", clientInfo);
             // send nicks of all connected clients to the new client
+
             if (connectedClients.Count > 1)
+            {
+                Console.WriteLine($"connectedClients.Values: {string.Join(", ", connectedClients.Values)}");
                 client.Send(Encoding.ASCII.GetBytes($"<|SRV|>Connected clients: {string.Join(", ", connectedClients.Values)}\n"));
+
+            }
+            client.Send(Encoding.ASCII.GetBytes("<|ACK|>"));
 
 
             byte[] buffer = new byte[1024];
@@ -68,63 +112,74 @@ namespace ChatServer
                     Console.WriteLine($"Received message: {message}");
                     // append nick to the message and send to all clients
                     message = $"{nickname}: {message}";
-                    SendToAllClients(message, client);
-
+                    SendToAllClients(message, clientInfo);
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex.Message}");
             }
-            finally
+
+            DisconnectClient(clientInfo);
+        }
+        static void HandleUdpClient(ClientInfo clientInfo, Socket udpSocket)
+        {
+            EndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            byte[] buffer = new byte[1024];
+
+            while (true)
             {
-                DisconnectClient(client);
+                int bytesRead = udpSocket.ReceiveFrom(buffer, ref clientEndPoint);
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Received UDP message: {message} from port {((IPEndPoint)clientEndPoint).Port}");
+                // find the client that sent the message (where key.UdpEndpoint == clientEndPoint)
+                ClientInfo? senderInfo = connectedClients.Keys.FirstOrDefault(c => c.UdpEndpoint.Equals(clientEndPoint));
+
+                if (senderInfo == null)
+                    continue;
+                message = $"{connectedClients[senderInfo]}: {message}";
+                // Send the UDP message to all other connected clients
+                SendToAllClients(message, senderInfo, udpSocket);
             }
-
-            Console.WriteLine($"Client disconnected from {((IPEndPoint?)client.RemoteEndPoint)?.Address}");
-
-            // remove the client from the list of connected clients
-            connectedClients.Remove(client);
-
-            client.Shutdown(SocketShutdown.Both);
-            client.Close();
         }
 
-        private static void DisconnectClient(Socket client)
+
+        private static void DisconnectClient(ClientInfo clientInfo)
         {
+            Console.WriteLine($"Client {connectedClients[clientInfo]} disconnected from the server");
+            connectedClients.Remove(clientInfo);
+
             // check if the client is still connected
-            if (!client.Connected)
-            {
+            if (!clientInfo.TcpClient.Connected)
                 return;
-            }
-            // disconnect the client from the server
-            string? nickname;
-            lock (connectedClientsLock)
-            {
-                connectedClients.TryGetValue(client, out nickname);
-                connectedClients.Remove(client);
-            }
-            client.Shutdown(SocketShutdown.Both);
-            client.Close();
-            connectedClients.Remove(client);
-            Console.WriteLine($"Client {nickname} disconnected from {((IPEndPoint?)client.RemoteEndPoint)?.Address}");
+            Console.WriteLine($"here");
+
+            clientInfo.TcpClient.Shutdown(SocketShutdown.Both);
+            clientInfo.TcpClient.Close();
         }
 
-        static void SendToAllClients(string message, Socket? excludeClient = null)
+        static void SendToAllClients(string message, ClientInfo? excludedClient = null, Socket? udpSocket = null)
         {
             lock (connectedClientsLock)
             {
-                foreach (var client in connectedClients)
+                foreach (var clientInfo in connectedClients.Keys)
                 {
-                    if (client.Key != excludeClient)
+                    if (clientInfo.Equals(excludedClient))
+                        continue;
+                    //if udp socket is not null, send via udp, else send via tcp
+                    if (udpSocket != null)
                     {
-                        client.Key.Send(Encoding.ASCII.GetBytes(message));
+                        udpSocket.SendTo(Encoding.ASCII.GetBytes(message), clientInfo.UdpEndpoint);
+                    }
+                    else
+                    {
+                        clientInfo.TcpClient.Send(Encoding.ASCII.GetBytes(message));
                     }
                 }
             }
         }
 
         static readonly object connectedClientsLock = new object();
-        static readonly Dictionary<Socket, string> connectedClients = new Dictionary<Socket, string>();
+        static readonly Dictionary<ClientInfo, string> connectedClients = new Dictionary<ClientInfo, string>();
     }
 }
